63aa4f457b5ae8f4348d8baf84171c0c5e07a3da
ZagZ <132225101+ZagZx@users.noreply.github.com>
2024-02-26 07:05:27 -0300

    Atualizar o AutoClicker.py

--- AutoClicker.py
+++ AutoClicker.py
@@ -1,2 +1,12 @@
 import pyautogui
 import keyboard
+
+if keyboard.is_pressed('pagedown'):
+    ligado = True
+    while ligado:
+         pyautogui.click()
+         if keyboard.is_pressed('pagedown'):
+             ligado = False
+
+
+
